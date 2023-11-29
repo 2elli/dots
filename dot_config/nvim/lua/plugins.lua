8e5732b55bdd2dcb20e8d751193c854c017cd7a2
elliot <elliot@utdallas.edu>
2023-11-29 14:33:35 -0600

    isolate lsp plugin settings

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -19,18 +19,11 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     "nvim-lua/plenary.nvim",
     {
-    "neovim/nvim-lspconfig",
-    lazy = true,
-    dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
-    },
-    { "nvimtools/none-ls.nvim", lazy = true },
-    {
     "nvim-treesitter/nvim-treesitter",
     dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", },
     },
     "nvim-tree/nvim-web-devicons",
     "nvim-tree/nvim-tree.lua",
-    {"akinsho/bufferline.nvim", version = "*"},
     "nvim-lualine/lualine.nvim",
 
     { "hrsh7th/nvim-cmp"
@@ -58,7 +51,7 @@ require("lazy").setup({
       event = "VeryLazy",
       init = function()
         vim.o.timeout = true
-        vim.o.timeoutlen = 300
+        vim.o.timeoutlen = 400
       end,
       opts = {}
     },
@@ -67,50 +60,17 @@ require("lazy").setup({
     "RRethy/vim-illuminate",
     "lewis6991/gitsigns.nvim",
 
+    "ThePrimeagen/harpoon",
+
     "EdenEast/nightfox.nvim",
+
+    "RaafatTurki/hex.nvim",
 })
 
 vim.opt.background = "dark"
 vim.cmd.colorscheme "carbonfox"
 
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-local lspconfig = require("lspconfig")
-lspconfig.rust_analyzer.setup({})
-lspconfig.clangd.setup({})
-lspconfig.pyright.setup({})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-    -- vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workleader_folder, opts)
-    -- vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workleader_folder, opts)
-    -- vim.keymap.set("n", "<leader>wl", function()
-    --   print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
-    -- end, opts)
-    -- vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-    -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    -- vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    -- vim.keymap.set("n", "<leader>f", function()
-    -- vim.lsp.buf.format { async = true }
-    -- end, opts)
-  end,
-})
-
 require("nvim-tree").setup()
-
-require("bufferline").setup()
 
 require("lualine").setup({
     options = {
@@ -171,6 +131,8 @@ require('gitsigns').setup()
 require('Comment').setup()
 
 require("dressing").setup()
+
+require("harpoon").setup()
 
 local wk = require("which-key")
 wk.register(mappings, opts)
