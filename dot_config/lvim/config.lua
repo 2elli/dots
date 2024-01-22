b7cd58bbe1e6533e1c470e05b2288d0181b4a896
elliot <elliot@utdallas.edu>
2024-01-22 12:44:25 -0600

    add harpoon to lvim

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -81,7 +81,22 @@ lvim.plugins = {
       }
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    config = function()
+        require("harpoon").setup()
+    end,
+    },
 }
+
+-- harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-b>", function() ui.nav_next() end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_prev() end)
+
 
 lvim.builtin.which_key.mappings["S"] = {
   name = "Session",
