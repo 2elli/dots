87f908f0d3e9fa5d82a14cc38e1b53e26ba3e7da
elliot <elliot@utdallas.edu>
2024-05-30 10:38:04 -0500

    update complete config and format

--- dot_config/nvim/lua/complete.lua
+++ dot_config/nvim/lua/complete.lua
@@ -17,38 +17,44 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {"nvim-lua/plenary.nvim"},
-    {"williamboman/mason.nvim"},
-    {"williamboman/mason-lspconfig.nvim"},
+    { "nvim-lua/plenary.nvim" },
 
-    {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
-    {"neovim/nvim-lspconfig"},
-    {"hrsh7th/cmp-nvim-lsp"},
-    {"hrsh7th/nvim-cmp"},
-    {"L3MON4D3/LuaSnip"},
-    {
-        "nvim-treesitter/nvim-treesitter",
-        dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+
+    { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/nvim-cmp" },
+    { "L3MON4D3/LuaSnip" },
+    { "nvim-treesitter/nvim-treesitter", dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", },
     },
-    {"nvim-tree/nvim-web-devicons"},
-    {"nvim-tree/nvim-tree.lua"},
-    {"nvim-lualine/lualine.nvim"},
     {
-        'numToStr/Comment.nvim',
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
         lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
     },
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    { "nvim-lualine/lualine.nvim" },
+    { "nvim-telescope/telescope.nvim",       tag = "0.1.6", },
+    { "numToStr/Comment.nvim",               lazy = false, },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {} },
     {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
         opts = {},
     },
-    {"stevearc/dressing.nvim"},
+    { "stevearc/dressing.nvim" },
 
-    {"RRethy/vim-illuminate"},
-    {"lewis6991/gitsigns.nvim"},
+    { "RRethy/vim-illuminate" },
+    { "lewis6991/gitsigns.nvim" },
 
-    {"ThePrimeagen/harpoon"},
+    { "ThePrimeagen/harpoon" },
 
     {
         "folke/persistence.nvim",
@@ -62,45 +68,41 @@ require("lazy").setup({
         end,
     },
 
-    {"EdenEast/nightfox.nvim"},
+    { "EdenEast/nightfox.nvim" },
 })
+
 
 vim.opt.background = "dark"
 vim.cmd.colorscheme "carbonfox"
 
--- LSP
-local lsp_zero = require('lsp-zero')
+
+--- LSP ---
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
-
-    vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end)
-    vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end)
-    vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end)
+    -- :help lsp-zero-keybindings
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    -- Replace the language servers listed here 
-    -- with the ones you want to install
-    ensure_installed = {'pyright', 'rust_analyzer', 'clangd'},
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = { "pyright", "rust_analyzer", "clangd" },
     handlers = {
         lsp_zero.default_setup,
     },
 })
 
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
 cmp.setup({
-  mapping = {
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-  }
+    mapping = {
+        ["<Tab>"] = cmp_action.tab_complete(),
+        ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
+    }
 })
---
+-----------
+
 
 require("nvim-tree").setup()
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
@@ -113,7 +115,7 @@ require("lualine").setup({
     },
 })
 
-require("ibl").setup( { scope = { enabled = false} } )
+require("ibl").setup({ scope = { enabled = false } })
 require("nvim-autopairs").setup()
 require("gitsigns").setup()
 require("Comment").setup()
@@ -124,15 +126,27 @@ require("harpoon").setup()
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>f", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-n>", function() ui.nav_next() end)
-vim.keymap.set("n", "<C-S-N>", function() ui.nav_prev() end)
---
+-- fzf
+local builtin = require("telescope.builtin")
+
+---- keybinds ----
+-- lsp
+vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end)
+vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end)
+vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end)
+
+-- fzf
+vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
 
 -- persistence
 vim.keymap.set("n", "<leader>Sc", function() require("persistence").load() end)
-vim.keymap.set("n", "<leader>Sl", function() require("persistence").load({ last = true}) end)
+vim.keymap.set("n", "<leader>Sl", function() require("persistence").load({ last = true }) end)
 vim.keymap.set("n", "<leader>Sq", function() require("persistence").stop() end)
---
+
+-- harpoon
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>f", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-n>", function() ui.nav_next() end)
+vim.keymap.set("n", "<C-S-N>", function() ui.nav_prev() end)
+------------------
