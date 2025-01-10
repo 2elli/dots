7942132fb33c7c2b8322dd7ef60048d5b6fd9249
elliot <elliot@ivy.rip>
2025-01-10 17:07:05 -0600

    nvim organize keybinds and change module calling convention

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -4,8 +4,8 @@ local M = {}
 M.builtin_binds = function()
     vim.g.mapleader = " "
     vim.keymap.set("n", "<SPACE>", "<Nop>")
-    vim.keymap.set("n", "<leader>w", "<CMD>w<cr>")
-    vim.keymap.set("n", "<leader>q", "<CMD>x<cr>")
+    vim.keymap.set("n", "<leader>w", "<CMD>w<CR>")
+    vim.keymap.set("n", "<leader>q", "<CMD>x<CR>")
     -- append next line
     vim.keymap.set("n", "J", "mzJ`z")
     -- keep page dwn + up centered
@@ -15,12 +15,12 @@ M.builtin_binds = function()
     vim.keymap.set("n", "n", "nzzzv")
     vim.keymap.set("n", "N", "Nzzzv")
     -- clear search highlight
-    vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+    vim.keymap.set("n", "<Esc>", "<CMD>nohlsearch<CR>")
     -- meta + j|k to move selection
-    vim.keymap.set("v", "<M-j>", "<CMD>m '>+1<CR>gv=gv")
-    vim.keymap.set("v", "<M-k>", "<CMD>m '<-2<CR>gv=gv")
-    vim.keymap.set("n", "<M-j>", "<CMD>m .+1<CR>==")
-    vim.keymap.set("n", "<M-k>", "<CMD>m .-2<CR>==")
+    vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")
+    vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")
+    vim.keymap.set("n", "<M-j>", ":m .+1<CR>==")
+    vim.keymap.set("n", "<M-k>", ":m .-2<CR>==")
     -- delete without copying to register
     vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
     -- tabs
@@ -44,45 +44,48 @@ M.lsp_binds = function(opts)
     vim.keymap.set({ "n", "v" }, "<leader>lf", function() vim.lsp.buf.format() end, opts)
 end
 
----Sets keybinds for all plugins
----@param plugins {harpoon: table, telescope_builtin: table}  # table of any plugins that need to be called directly in keymaps
-M.plugin_binds = function(plugins)
-    local harpoon = plugins.harpoon
-    local telescope_builtin = plugins.telescope_builtin
-
+---Sets keybinds for all plugins, params are plugins that need to be required and passed in for calls
+---@param harpoon table
+---@param oil table
+---@param scissors table
+---@param telescope_builtin table  # require("telescope.builtin")
+M.plugin_binds = function(harpoon, oil, scissors, telescope_builtin)
     -- harpoon
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<leader>f", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
     vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
 
-    -- trouble
-    vim.keymap.set("n", "<leader>xx", "<CMD>Trouble diagnostics toggle filter.buf=0<cr>")
-    vim.keymap.set("n", "<leader>xX", "<CMD>Trouble diagnostics toggle<cr>")
-
     -- scissors
-    vim.keymap.set("n", "<leader>se", function() require("scissors").editSnippet() end)
-    vim.keymap.set({ "n", "x" }, "<leader>sa", function() require("scissors").addNewSnippet() end)
+    vim.keymap.set("n", "<leader>se", function() scissors.editSnippet() end)
+    vim.keymap.set({ "n", "x" }, "<leader>sa", function() scissors.addNewSnippet() end)
 
     -- toggle oil file "tree"
-    vim.keymap.set("n", "<leader>e", function() vim.cmd((vim.bo.filetype == 'oil') and 'bd' or 'Oil') end)
+    vim.keymap.set("n", "<leader>e", function() oil.toggle_float() end)
 
     -- telescope and fzf
-    vim.keymap.set("n", "<leader>T", telescope_builtin.builtin, {})
-    vim.keymap.set("n", "<leader>\"", telescope_builtin.registers, {})
     vim.keymap.set("n", "<leader>/", telescope_builtin.live_grep, {})
     vim.keymap.set("n", "<leader>F", telescope_builtin.find_files, {})
+    vim.keymap.set("n", "<leader>tt", telescope_builtin.builtin, {})
+    vim.keymap.set("n", "<leader>t\"", telescope_builtin.registers, {})
+    vim.keymap.set("n", "<leader>tb", telescope_builtin.buffers, {})
+    vim.keymap.set("n", "<leader>tq", telescope_builtin.quickfix, {})
+    vim.keymap.set("n", "<leader>tj", telescope_builtin.jumplist, {})
+
+    -- trouble
+    vim.keymap.set("n", "<leader>xx", "<CMD>Trouble diagnostics toggle filter.buf=0<CR>")
+    vim.keymap.set("n", "<leader>xX", "<CMD>Trouble diagnostics toggle<CR>")
 
     -- git
-    vim.keymap.set("n", "<leader>gg", "<CMD>Gitsigns<cr>")
-    vim.keymap.set("n", "<leader>gb", "<CMD>Gitsigns blame_line<cr>")
-    vim.keymap.set("n", "<leader>gB", "<CMD>Gitsigns blame<cr>")
-    vim.keymap.set("n", "<leader>gd", "<CMD>vert rightb Gitsigns diffthis<cr>")
-    vim.keymap.set("n", "<leader>gn", "<CMD>Gitsigns next_hunk<cr>")
-    vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns prev_hunk<cr>")
+    vim.keymap.set("n", "<leader>gg", "<CMD>Gitsigns<CR>")
+    vim.keymap.set("n", "<leader>gb", "<CMD>Gitsigns blame_line<CR>")
+    vim.keymap.set("n", "<leader>gB", "<CMD>Gitsigns blame<CR>")
+    vim.keymap.set("n", "<leader>gd", "<CMD>vert rightb Gitsigns diffthis<CR>")
+    vim.keymap.set("n", "<leader>gn", "<CMD>Gitsigns next_hunk<CR>")
+    vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns prev_hunk<CR>")
 
     -- marks
-    vim.keymap.set("n", "<leader>m", "<CMD>MarksListAll<cr>")
+    vim.keymap.set("n", "<leader>m", "<CMD>MarksListAll<CR>")
 
     --[[ binds automatically setup by Comment.nvim
     NORMAL
