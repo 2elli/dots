47e24eb98ed1c98f55b7d2463bfb40249fff6e2e
elliot <elliot@ivy.rip>
2025-01-14 16:45:28 -0600

    nvim update cmds to be module

--- dot_config/nvim/lua/cmds.lua
+++ dot_config/nvim/lua/cmds.lua
@@ -1,9 +1,11 @@
+local M = {}
+
 -- auto commands --
--- setup lsp keymaps
-vim.api.nvim_create_autocmd("LspAttach", {
-    desc = "LSP actions and keymaps",
-    callback =
-        function(event)
+M.setup_autocmds = function()
+    -- setup lsp keymaps
+    vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "LSP actions and keymaps",
+        callback = function(event)
             local client = vim.lsp.get_client_by_id(event.data.client_id)
             local bufnr = event.buf
 
@@ -16,13 +18,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
                 vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
             end
         end,
-})
+    })
+end
 
 -- user commands --
-vim.api.nvim_create_user_command(
-    "Venv",
-    function()
-        vim.print(require("venv-selector").venv())
-    end,
-    { desc = "Get Current Python Venv", }
-)
+M.setup_usercmds = function()
+    vim.api.nvim_create_user_command(
+        "Venv",
+        function()
+            vim.print(require("venv-selector").venv())
+        end,
+        { desc = "Get Current Python Venv" }
+    )
+end
+
+-- generic cmds setup
+M.setup_cmds = function()
+    M.setup_autocmds()
+    M.setup_usercmds()
+end
+
+return M
